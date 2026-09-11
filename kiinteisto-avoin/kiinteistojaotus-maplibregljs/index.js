4ad028372e312fc1d60f198b3683e6a1c8b85e1c
nls-jajuko <nls-jajuko@users.noreply.github.com>
2026-09-11 13:10:33 +0300

    Update index.js

--- kiinteisto-avoin/kiinteistojaotus-maplibregljs/index.js
+++ kiinteisto-avoin/kiinteistojaotus-maplibregljs/index.js
@@ -1,6 +1,9 @@
 
-import { Map } from 'maplibre-gl'
+import { Map,setWorkerUrl } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css';
+import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
+
+setWorkerUrl(workerUrl);
 
 // let's load the style from project
 
